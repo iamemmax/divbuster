720036f13b7082dfd79f2a2934d233f2c87558d8
Eddie Jr <41637232+EdmundGiwa@users.noreply.github.com>
2024-06-03 11:22:09 +0100

    update

---
+++ src/app/(main)/learn-more/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <main className="wax-w-screen md:pb-20  bg-main min-h-screen">
+            <section className="bg-main  !mb-0 text-white shadow-sm">
+                <div>
+                    <button className='capitalize flex justify-center items-center rounded-full pl-6 pr-11 py-[14px] bg-[#34307A] bg-opacity-[20%] text-[14px] font-semibold gap-2 mt-28'> <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.9167 8.25016H10.0833V6.41683H11.9167M11.9167 15.5835H10.0833V10.0835H11.9167M11 1.8335C9.79623 1.8335 8.60423 2.0706 7.49208 2.53127C6.37993 2.99194 5.3694 3.66715 4.5182 4.51835C2.79911 6.23743 1.83334 8.56901 1.83334 11.0002C1.83334 13.4313 2.79911 15.7629 4.5182 17.482C5.3694 18.3332 6.37993 19.0084 7.49208 19.4691C8.60423 19.9297 9.79623 20.1668 11 20.1668C13.4312 20.1668 15.7627 19.2011 17.4818 17.482C19.2009 15.7629 20.1667 13.4313 20.1667 11.0002C20.1667 9.79638 19.9296 8.60438 19.4689 7.49223C19.0082 6.38008 18.333 5.36955 17.4818 4.51835C16.6306 3.66715 15.6201 2.99194 14.5079 2.53127C13.3958 2.0706 12.2038 1.8335 11 1.8335Z" fill="white" />
+                    </svg>
+                        about us</button>
+                    <div>
+                        <span>
+                            <p className='text-[42px] font-medium w-[642px] mt-1'>
+                                Liberty life - Health and Wealth for you.</p>
+                            <p className='text-[19px] font-normal text-[#CAC9D4] w-[499px]'>At Liberty life, we are committed at providing premium
+                                health coverage for all our customers.</p>
+                            <button className='capitalize flex justify-center items-center rounded-full py-3 px-12 mt-28 bg-[#34307A] bg-opacity-[20%] text-[14px] font-semibold gap-2'>benefits</button>
+                        </span>
+                        <span></span>
+                    </div>
+                    <div className='grid grid-cols-2 mt-8 gap-3'>
+                        <span className='bg-[#080D27] border-[0.3px] rounded-[10px]  px-6 py-6 border-[#407cff59]'>
+                            <p className='flex gap-4 text-[38px] font-bold'>01<span className='text-[15px] font-medium py-4'>premium hospital</span></p>
+                            <p className='text-[12px] font-normal w-[438px]'>Liberty life gives you premium hospitals with good healthcare facilities in your preferred state and local government.You can pick from the numerous hospitals around you.</p>
+                        </span>
+                        <span className='bg-[#080D27] border-[0.3px] rounded-[10px]  px-6 py-6 border-[#407cff59]'>
+                            <p className='flex gap-4 text-[38px] font-bold'>02<span className='text-[15px] font-medium py-4'>Lifestyle Reward</span></p>
+                            <p className='text-[12px] font-normal w-[438px]'>Liberty life gives you premium hospitals with good healthcare facilities in your preferred state and local government.You can pick from the numerous hospitals around you.</p>
+                        </span>
+                        <span className='bg-[#080D27] border-[0.3px] rounded-[10px]  px-6 py-6 border-[#407cff59]'>
+                            <p className='flex gap-4 text-[38px] font-bold'>03<span className='text-[15px] font-medium py-4'>Premium Health Policy</span></p>
+                            <p className='text-[12px] font-normal w-[438px] '>Liberty life gives you premium hospitals with good healthcare facilities in your preferred state and local government.You can pick from the numerous hospitals around you.</p>
+                        </span>
+                        <span className='bg-[#080D27] border-[0.3px] rounded-[10px]  px-6 py-6 border-[#407cff59]'>
+                            <p className='flex gap-4 text-[38px] font-bold'>04<span className='text-[15px] font-medium py-4'>Affordable Plan</span></p>
+
+                            <p className='text-[12px] font-normal w-[438px]'>Liberty life gives you premium hospitals with good healthcare facilities in your preferred state and local government.You can pick from the numerous hospitals around you.</p>
+                        </span>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
