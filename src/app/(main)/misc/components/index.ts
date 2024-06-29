0ed9b1c8959651e78f922df897aa749f79faeb67
iamemmax <iam4emmax@gmail.com>
2024-06-29 23:04:36 +0100

    complete remital flow

--- src/app/(main)/misc/components/index.ts
+++ src/app/(main)/misc/components/index.ts
@@ -1,5 +1,4 @@
 export { default as FooterSection } from "./FooterSection";
 export { default as HeroCarousel } from "./HeroCarousel";
-export { default as DetailsRequestModal } from "./modals/DetailsRequestModal";
 
 export { HospitalImage } from "./HospitalImage";
