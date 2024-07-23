3d5d477d2675913600d76b2edcc27d2efbcdf120
Eddie Jr <41637232+EdmundGiwa@users.noreply.github.com>
2024-07-23 10:47:45 +0100

    UPDATE

--- src/app/(main)/misc/components/insurance/modals/remital/RemitalPlanModal.tsx
+++ src/app/(main)/misc/components/insurance/modals/remital/RemitalPlanModal.tsx
@@ -181,7 +181,7 @@ const RemitalPlanModal = ({
         </div>
       ) : (
         <Dialog open={openRemitalPlan}>
-          <DialogContent className="!overflow-hidden  min-h-[90vh] max-h-[97vh] px-4 w-full md:min-h-[55rem]">
+          <DialogContent className="!overflow-hidden  min-h-[90vh] max-h-screen px-4 w-full md:min-h-[55rem]">
             <div className="md:w-full flex justify-between items-center">
               <DialogHeader className="bg-[#1B1687] w-full !justify-between">
                 <DialogTitle className="text-[#fff] whitespace-nowrap">
