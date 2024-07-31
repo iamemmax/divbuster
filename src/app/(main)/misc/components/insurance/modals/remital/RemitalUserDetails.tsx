9c3aaf3408a10e8f888fb756eae5d104bdc7867a
Eddie Jr <41637232+EdmundGiwa@users.noreply.github.com>
2024-07-31 15:08:26 +0100

    Update RemitalUserDetails.tsx

--- src/app/(main)/misc/components/insurance/modals/remital/RemitalUserDetails.tsx
+++ src/app/(main)/misc/components/insurance/modals/remital/RemitalUserDetails.tsx
@@ -222,10 +222,6 @@ const RemitalUserDetails = ({
     );
   };
 
-  //   const filteredStates = uniqueStates.filter((state) =>
-  //     state?.toLowerCase().includes(globalFilter.toLowerCase())
-
-  // );
   const stateOptions = uniqueStates?.map((state) => ({
     value: state,
     label: state,
