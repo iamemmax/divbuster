3c3344c1f3e49bffbc274ac194c34aa95b413855
iamemmax <iam4emmax@gmail.com>
2024-08-04 11:44:42 +0100

    update referral flow

--- src/app/(dashboard)/comp/components/plans/api/fetchPercentagePrice.ts
+++ src/app/(dashboard)/comp/components/plans/api/fetchPercentagePrice.ts
@@ -1,35 +1,16 @@
 import { adminAxios } from "@/lib/axios";
-interface pecentageType {
+interface Percentagedata {
+  [planName: string]: {
+    [beneficiaryNumber: string]: number;
+  };
+}
+
+export interface PercentageType {
   percentage_data: Percentagedata;
   base_price: number;
 }
 
-interface Percentagedata {
-  family: Family;
-  individual: Individual;
-  corporate: Individual;
-}
-
-interface Individual {
-  '1': number;
-  '2': number;
-  '3': number;
-  '4': number;
-  '5': number;
-  '6': number;
-  '7': number;
-  '8': number;
-}
-
-interface Family {
-  '3': number;
-  '4': number;
-  '5': number;
-  '6': number;
-  '7': number;
-  '8': number;
-}
-export const getPecentage = async () => {
+export const getPecentage = async (): Promise<PercentageType> => {
   const { data } = await adminAxios.get(`package_plan_percentage/`);
-  return data as pecentageType;
+  return data as PercentageType;
 };
