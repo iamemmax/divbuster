7f883f064c25077919382c0db093f0129fe2dbbf
iamemmax <iam4emmax@gmail.com>
2024-08-10 16:50:26 +0100

    add make beneficiaries

--- src/app/(main)/misc/components/insurance/modals/referral/AddReferralPhoneNumber.tsx
+++ src/app/(main)/misc/components/insurance/modals/referral/AddReferralPhoneNumber.tsx
@@ -18,12 +18,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "@radix-ui/react-label";
 import { Input2 } from "@/components/core/Input2";
-import {
-  addCommasToNumber,
-  formatAxiosErrorMessage,
-  formatCurrency,
-  removeCommaFromPrice,
-} from "@/utils";
+import { formatAxiosErrorMessage, formatCurrency } from "@/utils";
 import { AxiosError } from "axios";
 import { useErrorModalState } from "@/hooks";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -149,13 +144,7 @@ const AddRemitalPhoneNumer = ({
               setPaymentData({
                 account_name: data?.account_name,
                 account_no: data?.account_no,
-                amount: formatCurrency(
-                  Number(
-                    removeCommaFromPrice(
-                      String(addCommasToNumber(data?.plan_details?.price))
-                    )
-                  )
-                ),
+                amount: formatCurrency(Number(data?.plan_details?.price)),
                 bank_name: data?.bank_name,
                 paystack_link: data?.paystack_link,
               });
@@ -189,13 +178,7 @@ const AddRemitalPhoneNumer = ({
             } else {
               setPaymentData({
                 account_name: data?.account_name,
-                amount: formatCurrency(
-                  Number(
-                    removeCommaFromPrice(
-                      String(addCommasToNumber(data?.plan_details?.price))
-                    )
-                  )
-                ),
+                amount: formatCurrency(Number(data?.plan_details?.price)),
                 account_no: data?.account_no,
                 bank_name: data?.bank_name,
                 paystack_link: data?.paystack_link,
