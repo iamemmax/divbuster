460cce3f4923c733270592117a489b02d6810594
iamemmax <iam4emmax@gmail.com>
2024-09-18 01:13:57 -0700

    fix maquee ui

--- src/app/(main)/misc/components/Marquee.tsx
+++ src/app/(main)/misc/components/Marquee.tsx
@@ -6,48 +6,48 @@ import Image from "next/image";
 const Marquee = () => {
   return (
     <>
-      <section className="z-50 fixed bottom-0 w-full bg-[#161D42] py-4 text-lg font-display">
+      <section className="z-50 fixed bottom-0 w-full bg-[#161D42] py-[.375rem] text-lg font-display">
         <div className="marquee-content">
-          <div className="marquee-list font-clash flex items-center gap-10 md:gap-20 xl:text-xl">
+          <div className="marquee-list font-clash flex items-center gap-20 xl:text-xl">
             <Image
-              width={90}
-              height={80}
+              width={50}
+              height={40}
               alt="logo"
               src={"/images/logos/libertyAssured.png"}
             />
             <Image
-              width={130}
-              height={130}
+              width={100}
+              height={90}
               alt="logo"
               src={"/images/logos/libertyPay.png"}
             />
             <Image
-              width={100}
-              height={80}
+              width={60}
+              height={50}
               alt="logo"
               src={"/images/logos/seed&pennies.png"}
             />
             <Image
-              width={90}
-              height={70}
+              width={60}
+              height={40}
               alt="logo"
               src={"/images/logos/paybox360.png"}
             />
             <Image
-              width={140}
-              height={80}
+              width={100}
+              height={50}
               alt="logo"
               src={"/images/logos/whispersms.png"}
             />
             <Image
-              width={160}
-              height={100}
+              width={110}
+              height={60}
               alt="logo"
               src={"/images/logos/getlinked.png"}
             />
             <Image
-              width={60}
-              height={60}
+              width={30}
+              height={20}
               alt="logo"
               src={"/images/logos/winwise.png"}
             />
@@ -67,7 +67,7 @@ const Marquee = () => {
           align-items: center;
           justify-content: center;
           white-space: nowrap;
-          animation: marquee 40s linear infinite; /* Slowed down to 40 seconds */
+          animation: marquee 30s linear infinite; /* Slowed down to 40 seconds */
         }
 
         .marquee-content:hover .marquee-list {
@@ -90,7 +90,7 @@ const Marquee = () => {
           }
 
           .marquee-list {
-            gap: 20px;
+            gap: 30px;
           }
         }
       `}</style>
