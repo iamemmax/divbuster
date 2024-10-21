6f712ad63df84c9aa3c616c89ddba70108c69fda
Eddie Jr <41637232+EdmundGiwa@users.noreply.github.com>
2024-10-21 10:03:55 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,41 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Google Tag Manager Script NEW*/}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FP57HLE7JF"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics-ads"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FP57HLE7JF');
+            `,
+          }}
+        />
+
+
+        {/* <!-- Hotjar Tracking Code for Paybox360 --> */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5178477,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+          id="show-banner"
+        />
+
       </head>
 
       {/* <Script
