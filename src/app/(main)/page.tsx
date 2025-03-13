c07b80a821ae54748fe7741a5003acd9f08114bb
iamemmax <iam4emmax@gmail.com>
2025-03-13 10:16:14 +0100

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,12 +2,10 @@
 
 import { cn } from "@/utils/classNames";
 import { RightUpArrow } from "@/icons/core";
-import { Button, LinkButton } from "@/components/core";
+import { Button } from "@/components/core";
 
 import {
   CheckStar,
-  CrossIcon,
-  GroupIcon,
   HandIcon,
   LearnMore,
   Testimonial1,
@@ -38,6 +36,8 @@ import { useSearchParams } from "next/navigation";
 import GenerateReferralModal, { ReferralsuccessProp } from "./misc/components/insurance/modals/referral/GenerateReferralModal";
 import ReferralSuccessModal from "./misc/components/insurance/modals/referral/ReferralSuccessModal";
 import DoctorRoundedIcon from "./misc/icons/DoctorRounded";
+import HealthIcon from "./misc/icons/HealthIcon";
+import UserIcon from "./misc/icons/UserIcon";
 
 export default function Home() {
   const [openCheckPhoneNumberModal, setOpenCheckPhoneNumberModal] =
@@ -81,10 +81,36 @@ export default function Home() {
 
 
 
+  const cardArray1 =[
+    {
+      icon:<HealthIcon/>,
+      desc:"Over 1000+ Hospitals readily available based on your proximity."
+    },
+    {
+      icon:<UserIcon/>,
+      desc:"Premium health cover for both corporate and individuals users."
+    },
+  ]
+
+
+  const cardArray2 =[
+    {
+      title:"Lifetime  Rewards",
+      desc:"For every insurance plan you buy, you stand a chance to get a lifetime reward.",
+      button_name:"Learn more",
+    },
+    {
+      title:"Standard Hospitals",
+      desc:"We give you a standard hospitals in your preferred local government.",
+      button_name:"Learn more",
+    },
+  ]
+
+
   return (
-    <main className="md:pb-20 w-full bg-main min-h-screen">
+    <main className=" w-full bg-main ">
       <section className="bg-main w-full !mb-0 text-white shadow-sm  ">
-        <section className=" px-6 md:px-[3rem] 2xl:px-[7.5rem] py-[4.375rem] flex-col flex-wrap xl:flex-row flex ">
+        <section className=" px-6 md:px-[3rem] 2xl:px-[7.5rem] p-[4.375rem] pb-6 flex-col flex-wrap xl:flex-row flex ">
           <div className="flex flex-1 flex-col  gap-4  md:gap-6  max-md:py-10">
             <h6
               className={cn(
@@ -178,119 +204,39 @@ export default function Home() {
             {/* </DoctorRoundedIcon> */}
           </section>
         </section>
-        <section className="xl:px-[120px] xl:my-6">
-          <div
-            className={cn(
-              "px-3 2xl:px-6 flex flex-col items-stretch xl:grid xl:grid-cols-2 gap-4 rounded-[20px] xl:py-8 bg-[#FFFFFF08]"
-            )}
-          >
-            <div
-              className={cn(
-                "bg-[#1E2954] rounded-[20px] py-4 md:py-4 px-3 md:px-6 flex flex-col md:flex-row gap-4 md:gap-8"
-              )}
-            >
-              <div className="flex flex-col items-start justify-center">
-                <div className="border-[0.3px] flex flex-col items-center rounded-xl pt-3 pb-6 md:pb-0 px-11 md:px-[19px] border-[#475ffd54] bg-[#161D42]">
-                  <h3 className="font-sans text-base md:text-lg font-medium md:font-semibold text-white">
-                    Standard Hospitals
-                  </h3>
-                  <p className="text-xs md:text-sm text-center mt-2 font-sans text-[#CAC9D4]">
-                    Over 1000+ Hospitals readily available based on your
-                    proximity.
-                  </p>
-                  <div className="hidden md:flex gap-3 py-3">
-                    <LinkButton
-                      className="text bg-transparent px-3 py-2.5 text-white"
-                      color="#ffffff"
-                      href={`/faqs`}
-                    >
-                      Learn more <LearnMore />
-                    </LinkButton>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-start justify-center">
-                <div className="border-[0.3px] flex flex-col items-center rounded-xl pt-3 pb-6 md:pb-0 px-11 md:px-[19px] border-[#475ffd54] bg-[#161D42]">
-                  <h3 className="font-sans text-base md:text-lg font-medium md:font-semibold text-white">
-                    Comprehensive Plans
-                  </h3>
-                  <p className="text-xs md:text-sm text-center mt-2 font-sans text-[#CAC9D4]">
-                    Premium health cover for both corporate and individuals
-                    users.
-                  </p>
-                  <div className="hidden md:flex gap-3 py-3">
-                    <LinkButton
-                      className="text bg-transparent px-3 py-2.5 text-white"
-                      color="#ffffff"
-                      href={`/faqs`}
-                    >
-                      Learn more <LearnMore />
-                    </LinkButton>
-                  </div>
-                </div>
-              </div>
-              {/* bg-[#1E2954] rounded-[20px] py-4 md:py-8 px-4 md:px-8 flex items-start sm:items-center flex-col md:flex-row  gap-4 mb-16 md:mb-0 lg:gap-2 */}
-            </div>
+        <section className="px-4 md:px-[3rem] 2xl:px-[7.5rem]">
+        <div className="bg-[#10152e] rounded-[1.25rem] w-full md:p-6 grid-cols-1 md:grid-cols-2 grid gap-4">
+<div className="bg-[#1E2954] relative p-6 py-[2rem]  rounded-[20px]">
+  <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+{
+  cardArray1?.map((card,id:number)=>(
+    <div key={id} className="bg-[#161d42] py-11 px-[15px] rounded-lg flex justify-center items-center">
+      <div className=" absolute top-3 h-[2.625rem] w-[2.625rem] bg-[#161d42] flex justify-center items-center rounded-full  border-[0.3px] border-[#4760FD] border-opacity-55">
+        {card.icon}
+      </div>
+      <p className="text-sm text-white text-center">{card.desc}</p>
+    </div>
+  ))
+}
 
-<div className="bg-[#10152e] rounded-[1.25rem] p-6">
-            <div
-              className={cn(
-                "bg-[#1E2954] rounded-[20px] py-4 md:py-8 px-4 md:px-8 grid md:grid-cols-2 gap-4  md:mb-0 lg:gap-2"
-              )}
-            >
-              <div className="flex flex-col items-center justify-center">
-                <div className="border-[0.3px] flex flex-col items-center rounded-xl pt-3 pb-6 md:pb-0 px-11 md:px-[19px] border-[#475ffd54] bg-[#161D42]">
-                  <h3 className="font-sans text-base md:text-lg font-medium md:font-semibold text-white">
-                    Lifetime Rewards
-                  </h3>
-                  <p className="text-xs md:text-sm text-center mt-2 font-sans text-[#CAC9D4]">
-                    For every insurance plan you buy, you stand a chance to get
-                    a lifetime reward.
-                  </p>
-                  <div className="hidden md:flex gap-3 py-3">
-                    <LinkButton
-                      className="text bg-transparent px-3 py-2.5 text-white"
-                      color="#ffffff"
-                      href={`/faqs`}
-                    >
-                      Learn more <LearnMore />
-                    </LinkButton>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-start justify-center">
-                <div className="border-[0.3px] flex flex-col items-center rounded-xl pt-3 pb-6 md:pb-0 px-5 md:px-[10px] border-[#475ffd54] bg-[#161D42]">
-                  <h3 className="font-sans text-base md:text-lg font-medium md:font-semibold text-white">
-                    Hassle free registration
-                  </h3>
-                  <p className="text-xs md:text-sm text-center mt-2 font-sans text-[#CAC9D4]">
-                    Simply dial the USSD code, *347*180*9#, to connect to one of our agents and take the first step toward better health insurance services
-                  </p>
-                  <div className="hidden md:flex gap-3 py-1">
-                    <LinkButton
-                      className="text bg-transparent px-3 py-2.5 text-white"
-                      color="#ffffff"
-                      href={`/faqs`}
-                    >
-                      Learn more <LearnMore />
-                    </LinkButton>
-                  </div>
-                </div>
-              </div>
-              <div className="flex md:hidden gap-3">
-                <LinkButton
-                  className="text bg-transparent px-3 py-2.5 text-white"
-                  color="#ffffff"
-                  href={`/faqs`}
-                >
-                  Learn more <LearnMore />
-                </LinkButton>
-              </div>
-            </div>
-
+  </div>
 </div>
 
-          </div>
+<div className="bg-[#1E2954] p-6 py-[2rem]  rounded-[20px]">
+  <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+{
+  cardArray2?.map((card,id:number)=>(
+    <div key={id} className="bg-[#161d42] py-4 px-6 xl:px-[1.6rem] rounded-lg flex flex-col justify-center items-center">
+      <h3 className="text-lg font-medium text-white">{card?.title}</h3>
+      <p className="text-sm text-[#CAC9D4] text-center">{card.desc}</p>
+      <Button className="bg-transparent cursor-pointer pb-0 z-[999999] mt-1">{card?.button_name}<LearnMore/></Button>
+    </div>
+  ))
+}
+
+  </div>
+</div>
+        </div>
         </section>
       </section>
       {/* <Marquee /> */}
