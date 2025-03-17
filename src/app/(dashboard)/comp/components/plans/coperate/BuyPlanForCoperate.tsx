d31efed1656d67ae2bdd92762d16ead75b0f4ed2
iamemmax <iam4emmax@gmail.com>
2025-03-17 18:17:41 +0100

    update

--- src/app/(dashboard)/comp/components/plans/coperate/BuyPlanForCoperate.tsx
+++ src/app/(dashboard)/comp/components/plans/coperate/BuyPlanForCoperate.tsx
@@ -172,7 +172,7 @@ const BuyPlanModalForCoperate = ({
 
 {errors?.beneficiaries?.[index]?.name_of_beneficiary && (
                 <p className="text-red-700 text-xs mt-1">
-                  {errors?.beneficiaries?.[index]?.name_of_beneficiary.message}
+                  {errors?.beneficiaries?.[index]?.name_of_beneficiary?.message}
                 </p>
               )}
                         </div>
@@ -229,9 +229,9 @@ const BuyPlanModalForCoperate = ({
                                                   )}
                                                 />
                          
-                          {errors?.beneficiaries?.[index]?.phone_number_of_beneficiary && (
+                         {errors?.beneficiaries?.[index]?.phone_number_of_beneficiary && (
                 <p className="text-red-700 text-xs mt-1">
-                  {errors?.beneficiaries?.[index]?.phone_number_of_beneficiary.message}
+                  {errors?.beneficiaries?.[index]?.phone_number_of_beneficiary?.message}
                 </p>
               )}
                           </div>
