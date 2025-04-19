32277057e9dd6a7f09fb6371027e0486a6471a6e
iamemmax <iam4emmax@gmail.com>
2025-04-19 09:18:43 +0100

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -47,7 +47,7 @@ const Page = () => {
       </motion.section>
       {/* Section 2 - About */}
       <motion.section
-        className="snap-start min-h-screen  flex items-center justify-center px-4 md:px-[2rem] pb-5 xl:py-[50px] xl:px-[4.5rem] bg-[#0C0A3A]"
+        className="snap-start min-h-screen  flex items-center justify-center px-4 md:px-[2rem] pb-5 xl:py-[50px] bg-[#0C0A3A] xl:px-[4.5rem]"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
@@ -55,8 +55,9 @@ const Page = () => {
       >
         <BenefitsSection />
       </motion.section>
+      <div className="">
       <motion.section
-        className="snap-start min-h-screen bg-[url('/images/homepage/map.svg')]  rounded-t-md bg-cover  bg-no-repeat bg-opacity-50  flex items-center justify-center"
+        className="snap-start w-full flex items-center justify-center bg-[#0C0A3A]"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
@@ -64,6 +65,8 @@ const Page = () => {
       >
         <StepSection />
       </motion.section>
+
+      </div>
       <motion.section
         className="snap-start    flex items-center  bg-[url('/images/homepage/trending-page-bg.svg')] z-[999999]   bg-no-repeat bg-cover  px-4 md:px-[2rem] xl:px-[4.5rem] justify-center"
         initial={{ opacity: 0, y: 40 }}
