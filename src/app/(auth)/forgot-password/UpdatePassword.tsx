702aa3405c8db46fa4bb513feb5e4ee2b01ab2e6
iamemmax <iam4emmax@gmail.com>
2025-12-31 15:44:00 +0100

    fix issues reported by qa

--- src/app/(auth)/forgot-password/UpdatePassword.tsx
+++ src/app/(auth)/forgot-password/UpdatePassword.tsx
@@ -190,12 +190,19 @@ const UpdatePassword = ({ email, goToPreviousStep, isFirstStep = false, isLastSt
               className="w-full h-full px-3 py-2 border-none outline-none  text-sm font-archivo rounded-lg"
               {...register("password")}
             />
-            <button
+             <button
               type="button"
-              className="absolute inset-y-0 right-0 pr-3 flex items-center"
+              className="absolute inset-y-0 right-0 pr-3 flex items-center text-black dark:text-white"
               onClick={() => setShowPassword(!showPassword)}
             >
-              <EyeIcon />
+              {!showPassword ? (
+                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                </svg>
+              ) : (
+                <EyeIcon className="text-black dark:text-white" />
+              )}
             </button>
           </div>
           {errors.password && (
@@ -223,12 +230,19 @@ const UpdatePassword = ({ email, goToPreviousStep, isFirstStep = false, isLastSt
               className="w-full h-full px-3 py-2 border-none outline-none  text-sm font-archivo rounded-lg"
               {...register("confirm_password")}
             />
-            <button
+             <button
               type="button"
-              className="absolute inset-y-0 right-0 pr-3 flex items-center"
+              className="absolute inset-y-0 right-0 pr-3 flex items-center text-black dark:text-white"
               onClick={() => setShowConfirmPassword(!showConfirmPassword)}
             >
-              <EyeIcon />
+              {!showConfirmPassword ? (
+                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                </svg>
+              ) : (
+                <EyeIcon className="text-black dark:text-white" />
+              )}
             </button>
           </div>
           {errors.confirm_password && (
